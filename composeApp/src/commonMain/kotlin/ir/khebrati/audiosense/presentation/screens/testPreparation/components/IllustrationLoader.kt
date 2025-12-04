124ed7a831b15524d09c89738aa881b7b5929c95
erfan <erfan>
2025-12-04 17:39:32 +0330

    load illustration image with coil

---
+++ composeApp/src/commonMain/kotlin/ir/khebrati/audiosense/presentation/screens/testPreparation/components/IllustrationLoader.kt
@@ -0,0 +1,74 @@
+package ir.khebrati.audiosense.presentation.screens.testPreparation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import audiosense.composeapp.generated.resources.Res
+import co.touchlab.kermit.Logger
+import coil3.ColorImage
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.AsyncImagePainter.*
+import coil3.compose.AsyncImagePainter.State.*
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
+import coil3.compose.SubcomposeAsyncImage
+import ir.khebrati.audiosense.presentation.components.LoadingScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun IllustrationLoader(
+    modifier: Modifier = Modifier,
+    illustrationFileName: String,
+    onLoaded: () -> Unit,
+    onLoading: () -> Unit,
+    painter: AsyncImagePainter
+){
+    Image(
+        painter = painter,
+        contentDescription = "something",
+        modifier = modifier
+    )
+//    AsyncImage(
+//        modifier = modifier,
+//        model = Res.getUri("drawable/${illustrationFileName}.svg"),
+//        contentDescription = "Beautiful illustration",
+//        onLoading = {
+//
+//        }
+//    )
+//    SubcomposeAsyncImage(
+//        modifier = modifier,
+//        model = Res.getUri("drawable/${illustrationFileName}.svg"),
+//        contentDescription = "Beautiful illustration",
+//        loading = {
+//            LoadingScreen()
+//        }
+//    )
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Preview
+@Composable
+fun IllustrationLoaderPreview(){
+    val previewHandler = AsyncImagePreviewHandler {
+        ColorImage(Color.Red.toArgb())
+    }
+    CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler){
+//        IllustrationLoader()
+    }
+}
