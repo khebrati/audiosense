099aa3f90d0f8510ef40554c104e2e078c68b025
erfan <erfan>
2025-12-04 18:51:34 +0330

    fix layout of the illustrations

--- composeApp/src/commonMain/kotlin/ir/khebrati/audiosense/presentation/screens/testPreparation/components/TestSetupLayout.kt
+++ composeApp/src/commonMain/kotlin/ir/khebrati/audiosense/presentation/screens/testPreparation/components/TestSetupLayout.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -21,9 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import audiosense.composeapp.generated.resources.Res
 import coil3.compose.AsyncImagePainter
-import coil3.compose.ImagePainter
 import coil3.compose.rememberAsyncImagePainter
-import coil3.compose.rememberConstraintsSizeResolver
 import ir.khebrati.audiosense.presentation.components.AudiosenseAppBar
 import ir.khebrati.audiosense.presentation.components.AudiosenseScaffold
 import ir.khebrati.audiosense.presentation.components.LoadingScreen
@@ -34,22 +35,24 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun TestSetupLayout(
     modifier: Modifier = Modifier,
     title: String,
+    illustrationName: String,
     onNavigateBack: () -> Unit,
-    content: @Composable ColumnScope.() -> Unit,
+    content: @Composable () -> Unit,
 ) {
     var loading by remember { mutableStateOf(true) }
-    val painter = rememberAsyncImagePainter(
-        model = Res.getUri("drawable/QuestionGuy.svg"),
-        onState = {state ->
-            if(state is AsyncImagePainter.State.Success){
-                loading = false
-            }
-        }
-    )
+    val painter =
+        rememberAsyncImagePainter(
+            model = Res.getUri("drawable/$illustrationName.png"),
+            onState = { state ->
+                if (state is AsyncImagePainter.State.Success) {
+                    loading = false
+                }
+            },
+        )
     if (loading) {
         LoadingScreen()
     } else {
-        TestSetupReady(modifier, title, onNavigateBack, {  }, onLoading = {},painter,content)
+        TestSetupReady(modifier, title, onNavigateBack, painter, content)
     }
 }
 
@@ -58,10 +61,8 @@ private fun TestSetupReady(
     modifier: Modifier = Modifier,
     title: String,
     onNavigateBack: () -> Unit,
-    onLoaded: () -> Unit,
-    onLoading: () -> Unit,
     painter: AsyncImagePainter,
-    content: @Composable (ColumnScope.() -> Unit),
+    content: @Composable () -> Unit,
 ) {
     AudiosenseScaffold(
         contentPadding = PaddingValues(),
@@ -79,13 +80,10 @@ private fun TestSetupReady(
         Column(modifier = modifier.verticalScroll(rememberScrollState())) {
             Box(
                 modifier =
-                    Modifier.background(MaterialTheme.colorScheme.primaryContainer).padding(horizontal = 50.dp, vertical = 28.dp)
+                    Modifier.background(MaterialTheme.colorScheme.primaryContainer).padding(horizontal = 70.dp)
             ) {
                 IllustrationLoader(
                     modifier = Modifier.fillMaxWidth(),
-                    illustrationFileName = "QuestionGuy",
-                    onLoaded = onLoaded,
-                    onLoading = onLoading,
                     painter = painter
                 )
             }
@@ -97,5 +95,13 @@ private fun TestSetupReady(
 @Preview(widthDp = 400, heightDp = 800)
 @Composable
 fun TestSetupLayoutPreview() {
-    AppTheme { Surface { TestSetupLayout(title = "Ready for test", onNavigateBack = {}) {} } }
+    AppTheme {
+        Surface {
+            TestSetupLayout(
+                title = "Ready for test",
+                onNavigateBack = {},
+                illustrationName = "Question",
+            ) {}
+        }
+    }
 }
