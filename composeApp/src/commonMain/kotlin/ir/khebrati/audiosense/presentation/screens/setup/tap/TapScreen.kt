5b1dc7885e0dd52c625ca069000749ef3b75e9de
erfan <erfan>
2025-12-09 19:55:04 +0330

    Touch for feedback screen

--- composeApp/src/commonMain/kotlin/ir/khebrati/audiosense/presentation/screens/setup/tap/TapScreen.kt
+++ composeApp/src/commonMain/kotlin/ir/khebrati/audiosense/presentation/screens/setup/tap/TapScreen.kt
@@ -1,9 +1,22 @@
 package ir.khebrati.audiosense.presentation.screens.setup.tap
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.VolumeUp
+import androidx.compose.material.icons.filled.GraphicEq
+import androidx.compose.material.icons.filled.TouchApp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ir.khebrati.audiosense.presentation.navigation.AudiosenseRoute.*
 import ir.khebrati.audiosense.presentation.screens.setup.components.TestSetupLayout
+import ir.khebrati.audiosense.presentation.screens.setup.components.TipCard
 import ir.khebrati.audiosense.presentation.screens.setup.navigation.SetupInternalRoute
 import ir.khebrati.audiosense.presentation.screens.setup.navigation.SetupInternalRoute.*
 import org.jetbrains.compose.resources.painterResource
@@ -22,5 +35,20 @@ fun TapScreen(
         pagerState = pagerState,
         onClickNext = { onNavigateSelectDevice(SelectDeviceRoute) },
         onClickSkip = {},
-    ) {}
+    ) {
+        Row {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(14.dp)
+            ) {
+                Spacer(modifier = Modifier.height(14.dp))
+                TipCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    icon = Icons.Default.TouchApp,
+                    iconDescription = "Touch on app",
+                    title = "Touch for feedback",
+                    body = "You will hear faint tones. Tap the screen anywhere as soon as you hear a sound.",
+                )
+            }
+        }
+    }
 }
